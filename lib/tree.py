f668bb69fcbec1e1ce47640dc0c40cdc5e4b1321
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-12-15 09:03:57 -0500

    done

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,44 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        # uses depth first
+        queue = [self.root]
+
+        while len(queue):
+            node = queue.pop(0)
+            if node["id"] == id:
+                return node
+            queue = node["children"] + queue
+
+        return None
+
+    def breadth_first_traversal(node):
+        result = []
+        nodes_to_visit = [node]
+
+        while len(nodes_to_visit):
+            current = nodes_to_visit.pop(0)
+
+            result.append(current["value"])
+            nodes_to_visit += current["children"]
+
+        return result
+
+    def depth_first_traversal(node):
+        result = []
+        nodes_to_visit = [node]
+
+        while len(nodes_to_visit):
+            current = nodes_to_visit.pop(0)
+            result.append(current["value"])
+            nodes_to_visit = node["children"] + nodes_to_visit
+
+        return result
+
+    def depth_first_recursive(node, result=[]):
+        result.append(node["value"])
+        for child in node["children"]:
+            Tree.depth_first_recursive(child)
+        return result
